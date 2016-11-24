ca5244f2754939d21f11825193de2db0ec11e6f8
daviddk222 <danielwinkel4@hotmail.com>
2016-11-24 15:49:10 +0100

    updated front-end to display all fields of product object - updated category converter to work with all dba section categories - updated category converter tests to work with update

--- src/main/webapp/app/view1/view1.js
+++ src/main/webapp/app/view1/view1.js
@@ -1,55 +1,46 @@
 'use strict';
 angular.module('myApp.view1', ['ngRoute'])
 
-        .config(['$routeProvider', function ($routeProvider) {
-                $routeProvider.when('/view1', {
-                    templateUrl: 'app/view1/view1.html',
-                    controller: 'View1Ctrl',
-                });
-            }])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/view1', {
+            templateUrl: 'app/view1/view1.html',
+            controller: 'View1Ctrl',
+        });
+    }])
 
-        .controller('View1Ctrl', ["InfoFactory", "InfoService", '$scope','$http', function (InfoFactory, InfoService, $scope, $http) {
-                $scope.msgFromFactory = InfoFactory.getInfo();
-                $scope.msgFromService = InfoService.getInfo();
-                $scope.getInfo = function () {
-                    $http({
-                        url: 'http://localhost:8084/api/api/products/cars/Audi',
-                        method: 'GET'
-                    })
-                            .success(function (data, status, headers, config) {
-                                console.log(data);
-                                $scope.data = data;
-                                console.log($scope.data);
-                            })
-                            .error(function (data, status, headers, config) {
-                                console.log("Error " + data);
-                            });
+    .controller('View1Ctrl', ["InfoFactory", "InfoService", '$scope', '$http', function (InfoFactory, InfoService, $scope, $http) {
+
+        $scope.msgFromFactory = InfoFactory.getInfo();
+        $scope.msgFromService = InfoService.getInfo();
+        $scope.category = "undefined";
+
+        $scope.getResults = function (searchText, category) {
+            console.log(searchText);
+            console.log(category);
+
+            if (angular.isUndefined(category) || category === "undefined") {
+                category = "";
+            } else {
+                category = category + "/";
+            }
+
+            $http({
+                url: 'http://localhost:8084/api/api/products/' + category + searchText,
+                method: 'GET'
+            })
+                .success(function (data, status, headers, config) {
+                    console.log(data);
+                    $scope.data = data;
+                    console.log($scope.data);
+                })
+                .error(function (data, status, headers, config) {
+                    console.log("Error " + data);
+                });
 
 //                    productsFactory.getInfo().then(function (response) {
 //                        $scope.data = response.data;
 //                    });
-                }
+        };
 
-            }])
+    }]);
 
-        .factory('productsFactory', ['$http', function ($http) {
-                var getInfo = function () {
-                    return $http({
-                        url: 'http://localhost:8084/api/products',
-                        method: 'GET'
-                    })
-                            .success(function (data, status, headers, config) {
-                                console.log(data);
-                                if (data.error) {
-                                    alert("Error: " + data.message);
-                                }
-                                return data;
-                            })
-                            .error(function (data, status, headers, config) {
-                                return console.log("Error " + data);
-                            });
-                };
-                return {
-                    getInfo: getInfo
-                };
-            }]);
