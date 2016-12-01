7784905dd8ace61909da40d8f95965c7c8302054
Mikkel Ziemer <mzhjensen@gmail.com>
2016-12-01 14:32:15 +0100

    Cleaned up API key file

--- src/main/java/org/school/ezon/api/APIKeys.java
+++ src/main/java/org/school/ezon/api/APIKeys.java
@@ -5,24 +5,6 @@
  */
 package org.school.ezon.api;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.client.Invocation;
-import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import org.apache.commons.codec.binary.Base64;
-
 /**
  *
  * @author Mikkel
